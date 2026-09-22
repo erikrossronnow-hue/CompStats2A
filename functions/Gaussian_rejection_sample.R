8407d0c6137721b0f6cf1c0e7483ad24fb56cbed
marie791791 <marieramm2@gmail.com>
2026-09-22 13:02:18 +0200

    commit

--- functions/Gaussian_rejection_sample.R
+++ functions/Gaussian_rejection_sample.R
@@ -1,22 +1,24 @@
 # can write the gaussian stuff as a function here, test the function in the sandbox
 # with a new script please. 
-Gaussian_rejection_sample = function() {
-  return(0)
-}
 
 
 # mu
-mu <- # need to do something with roots
+mu <- uniroot(
+  sum(x * z) - sum(x * exp(y * x)),
+  interval = c(0, 10),
+  x = x,
+  z = z
+)$root
   
-  # sigma
-  sigma <- 1 / sqrt(sum(x^2 * exp(mu * x)))
+# sigma
+sigma <- 1 / sqrt(sum(x^2 * exp(mu * x)))
 
 # f function
 log_f <- function(y, x, z) {
   sum0 <- sum(y*x*z)
   sum1 <- sum(exp(y*x))
   sum0 - sum1
-}
+} # Need to add protection for y <0 ???
 
 # proposal function 
 log_q <- function(y, mu, sigma) {
@@ -25,7 +27,7 @@ log_q <- function(y, mu, sigma) {
 
 
 # Find M
-
+# Not quite sure how but probably also need to be some kind of maximizing function
 
 # Recall that the shape parameter has to fulfill: r >= 1
 gaussian_random <- function(N, mu, sigma, M, x, z) {
@@ -33,11 +35,9 @@ gaussian_random <- function(N, mu, sigma, M, x, z) {
   y <- rnorm(N, mean = mu, sd = sigma)
   u <- runif(N)
   
-  log_accept <- log_f(y, x, z) -
-    log_q(y, mu, sigma) -
-    log(M)
-  
-  accept <- y >= 0 & log(u) <= log_accept
+  accept <- y >= 0 & u <= exp(log_f(y, x, z) -
+                                     log_q(y, mu, sigma) -
+                                     log(M))
   
   y[accept]
 }
