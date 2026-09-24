c4b15a946f0e1f898169b2bde172563517c10d70
zhihao0539 <wang2297099267@gmail.com>
2026-09-24 13:07:34 +0200

    Now it works for general Gaussian

--- functions/Gaussian_envelope.R
+++ functions/Gaussian_envelope.R
@@ -9,9 +9,47 @@ log_f <- function(y) {
   sum(y * z * x - exp(y * x))
 }
 
-log_g <- function(y){
-  dnorm(y, mean=0, sd=1, log=TRUE)
+
+mu <- 0.24
+sigma <- 4e-2
+log_g <- function(y) {
+  dnorm(y, mean = mu, sd = sigma, log = TRUE)
 }
+
+y_grid <- seq(0, 1, length.out = 1000)
+
+# log_f evaluates one y at a time
+f_values <- vapply(y_grid, log_f, numeric(1))
+g_values <- log_g(y_grid)
+
+quartz(bg = "white", width = 8, height = 5)
+
+plot(
+  y_grid, f_values,
+  type = "l",
+  col = "blue",
+  lwd = 2,
+  xlim = c(0, 1),
+  ylim = range(c(f_values, g_values)),
+  xlab = "y",
+  ylab = "Log density"
+)
+
+lines(
+  y_grid, g_values,
+  col = "red",
+  lwd = 2,
+  lty = 2
+)
+
+legend(
+  "topright",
+  legend = c("log_f(y)", "log_g(y)"),
+  col = c("blue", "red"),
+  lty = c(1, 2),
+  lwd = 2,
+  bty = "n"
+)
 
 log_ratio <- function(y) {
   log_g(y) - log_f(y)
@@ -21,12 +59,13 @@ result <- optimize(
   f = log_ratio, interval = c(0,200)
 )
 
+result
 log_alpha <- result$objective
-
+log_alpha
 get_one_sample <- function(log_alpha){
   rejected <- 0
   repeat{
-    Y <- rnorm(1)
+    Y <- rnorm(1, mean = mu, sd = sigma)
     U <- runif(1)
 
     if(Y>=0 && log(U)<=log_alpha + log_f(Y) - log_g(Y)){
@@ -63,11 +102,6 @@ hist(
   col = "lightblue",
   border = "white",
   main = "Rejection Sampling",
-  xlab = "y"
+  xlab = "y",
+  plot = TRUE
 )
-
-sample_mean <- mean(samples)
-sample_variance <- var(samples)
-
-sample_mean
-sample_variance
